9289c17e0cf3041669a822ba87156540e7b0d4b3
Denise <denise@skidun.com.br>
2013-05-13 12:17:32 -0300

    folder deletar removido

--- prototipo-nao-copiar/footer.php
+++
@@ -1,7 +0,0 @@
-        <script src="js/vendor/jquery-1.9.1.min.js"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-    </body>
-</html>
