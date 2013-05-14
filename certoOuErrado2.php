d7d8940eeab43e7f7a4d0ac3bd52cccc022f1496
Denise <denise@skidun.com.br>
2013-05-14 19:10:51 -0300

    atualizando

---
+++ certoOuErrado2.php
@@ -0,0 +1,86 @@
+<?php require_once('header.php'); ?>
+
+		
+		<?php require_once('topo-dois.php'); ?>
+		
+		<div id="conteudo">
+			<div id="wrap">
+			
+				<div class="nav-perfil">
+					<a class="anterior" href="#"></a>
+					<a class="perguntas" href="#"></a>
+					<a class="faixasClassificacao ativo" href="#"></a>
+					<a class="customizacao" href="#"></a>
+					<a class="visualizacao" href="#"></a>
+					<a class="proximo" href="#"></a>
+				</div>
+			
+				<div id="accordion">
+					
+					<div class="group">
+							<div class="header">
+								<span class="icon"></span>
+								<div class="input"><input type="text" name="nome" value="Carente" size="" /></div>
+								<span class="arrow"></span>
+							</div>
+							<div class="body">
+								<div class="texto">
+									<label for="descricao">Descrição</label>
+									<div class="textarea"><textarea name="descricao" cols="" rows="">Você é a amiga da galera! Seu tempo livre é todo dedicado a amigos e pessoas queridas, por isso você nem acha que precisa gastar os neurônios pensando em como fazer para encontrar um namorado. Solidão? Que nada!</textarea></div>
+									<label for="link">Link de referência:</label>
+									<div class="input"><input type="text" name="link" value="http://www.gnt.com.br/post-falando-sobre-esse-perfil.html" size="" /></div>
+									<label for="texto">Texto do link de referência:</label>
+									<div class="input"><input type="text" name="texto" value="Saiba mais" size="" /></div>
+								</div>
+								<div class="imagem">
+									<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
+									<div class="quadro"><img id="alvo" src="assets/img/backgrounds/imagem.png" name="imagem" /></div>
+									
+									<span class="btn btn-success fileinput-button">
+										<input id="fileupload" type="file" />
+									</span>
+									
+								</div>
+							</div>
+					</div>	
+					
+					<div class="group">
+							<div class="header">
+								<span class="icon"></span>
+								<div class="input"><input type="text" name="nome" value="Carente" size="" /></div>
+								<span class="arrow"></span>
+							</div>
+							<div class="body">
+								<div class="texto">
+									<label for="descricao">Descrição</label>
+									<div class="textarea"><textarea name="descricao" cols="" rows="">Você é a amiga da galera! Seu tempo livre é todo dedicado a amigos e pessoas queridas, por isso você nem acha que precisa gastar os neurônios pensando em como fazer para encontrar um namorado. Solidão? Que nada!</textarea></div>
+									<label for="link">Link de referência:</label>
+									<div class="input"><input type="text" name="link" value="http://www.gnt.com.br/post-falando-sobre-esse-perfil.html" size="" /></div>
+									<label for="texto">Texto do link de referência:</label>
+									<div class="input"><input type="text" name="texto" value="Saiba mais" size="" /></div>
+								</div>
+								<div class="imagem">
+									<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
+									<div class="quadro"><img id="alvo" src="assets/img/backgrounds/imagem.png" name="imagem" /></div>
+									
+									<span class="btn btn-success fileinput-button">
+										<input id="fileupload" type="file" />
+									</span>
+									
+								</div>
+							</div>
+					</div>
+					
+				</div><!--accordion-->
+				
+				<div class="holder">
+					<a class="novo-perfil" href="#"></a>
+				</div>
+				
+				<a class="proxima-etapa" href="#"></a>
+			
+			</div>
+		</div>
+		
+
+<?php require_once('footer.php'); ?>
---
+++ respostaCerta1.php
@@ -0,0 +1,154 @@
+<?php require_once('header.php'); ?>
+		
+		<?php require_once('topo-dois.php'); ?>
+	
+		
+		<div id="conteudo" class="perfil2">
+			<div id="wrap">
+			
+				<div class="nav-perfil">
+					<a class="anterior" href="#"></a>
+					<a class="perfis" href="#"></a>
+					<a class="perguntas ativo" href="#"></a>
+					<a class="customizacao" href="#"></a>
+					<a class="visualizacao" href="#"></a>
+					<a class="proximo" href="#"></a>
+				</div>
+			
+				<div class="titulo-perguntas">Perguntas:</div>
+				
+				<div id="accordion2">
+					
+					<div class="group">
+							<div class="header">
+								<span class="icon"></span>
+								<div class="input"><input type="text" name="nome" value="Carente" size="" /></div>
+								<span class="arrow"></span>
+							</div>
+							<div class="body">
+							
+								<div id="perguntas">
+								
+									<div class="texto">
+										<label for="link">Link de referência:</label>
+										<div class="input"><input type="text" name="link" value="http://www.gnt.com.br/post-falando-sobre-esse-perfil.html" size="" /></div>
+										<label for="texto">Texto do link de referência:</label>
+										<div class="input"><input type="text" name="texto" value="Saiba mais" size="" /></div>
+									</div>
+									<div class="imagem">
+										<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
+										<div class="quadro"><img id="alvo0" src="assets/img/backgrounds/imagem.png" /></div>
+										
+										<form id="fileupload0" action="assets/server/php/" method="POST" enctype="multipart/form-data">
+										<span class="btn btn-success fileinput-button">
+											<input id="file" type="file"  />
+										</span>
+										</form>
+										
+									</div>
+								
+								</div><!--perguntas-->
+								
+								<div id="respostas">
+								
+									<div class="titulo-respostas">Respostas:</div>
+									
+									<div id="sortable0" class="sorteia">
+										
+												<div class="header">
+													<span class="icon"></span>
+													<div class="input"><input type="text" name="nome" value="Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cui..." size="" /></div>
+													<div class="radio">
+														<label for="radio00" class="radioCustom"></label>
+														<input type="radio" id="radio00" value="0" name="grupo0" />
+														Esta é a resposta correta
+													</div>
+												</div>
+												
+												<div class="header">
+													<span class="icon"></span>
+													<div class="input"><input type="text" name="nome" value="Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cui..." size="" /></div>
+													<div class="radio">
+														<label for="radio10" class="radioCustom"></label>
+														<input type="radio" id="radio10" value="0" name="grupo0" />
+														Esta é a resposta correta
+													</div>
+												</div>
+										
+									</div>
+
+									<a id="nova-resposta-respostaCerta" class="nova-resposta" href="javascript:void(0)"></a>
+																	
+								</div><!--respostas-->
+								
+							</div>
+					</div>
+					
+					<div class="group">
+							<div class="header">
+								<span class="icon"></span>
+								<div class="input"><input type="text" name="nome" value="" size="" /></div>
+								<span class="arrow"></span>
+							</div>
+							<div class="body">
+							
+								<div id="perguntas">
+								
+									<div class="texto">
+										<label for="link">Link de referência:</label>
+										<div class="input"><input type="text" name="link" value="" size="" /></div>
+										<label for="texto">Texto do link de referência:</label>
+										<div class="input"><input type="text" name="texto" value="" size="" /></div>
+									</div>
+									<div class="imagem">
+										<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
+										<div class="quadro"><img id="alvo1" src="assets/img/backgrounds/imagem.png" /></div>
+										
+										<form id="fileupload1" action="assets/server/php/" method="POST" enctype="multipart/form-data">
+										<span class="btn btn-success fileinput-button">
+											<input id="file" type="file"  />
+										</span>
+										</form>
+										
+									</div>
+								
+								</div><!--perguntas-->
+								
+								<div id="respostas">
+								
+									<div class="titulo-respostas">Respostas:</div>
+									
+									<div id="sortable1" class="sorteia">
+										
+												<div class="header">
+													<span class="icon"></span>
+													<div class="input"><input type="text" name="nome" value="" size="" /></div>
+													<div class="radio">
+														<label for="radio01" class="radioCustom"></label>
+														<input type="radio" id="radio01" value="0" name="grupo1" />
+														Esta é a resposta correta
+													</div>
+												</div>
+										
+									</div>
+
+									<a id="nova-resposta-respostaCerta" class="nova-resposta" href="javascript:void(0)"></a>
+																	
+								</div><!--respostas-->
+								
+							</div>
+					</div>
+					
+				</div><!--accordion-->			
+
+				<div class="holder">
+					<a id="nova-pergunta-respostaCerta" class="nova-pergunta" href="#"></a>
+				</div>
+				
+				<a class="proxima-etapa" href="#" rel="link-interno" title="próxima etapa"></a>
+			
+			</div>
+		</div>
+		
+
+<?php require_once('footer.php'); ?>
