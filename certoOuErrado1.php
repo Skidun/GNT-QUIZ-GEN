de2b59ea8934c0a02a4c6f68e9013aa13064ba54
Denise <denise@skidun.com.br>
2013-05-15 17:30:36 -0300

    certoOuErrado1.php verificado

--- certoOuErrado1.php
+++ certoOuErrado1.php
@@ -18,9 +18,10 @@
 				<div id="accordion2">
 					
 					<div class="group">
+							
 							<div class="header">
 								<span class="icon"></span>
-								<div class="input"><input type="text" name="nome" value="Carente" size="" /></div>
+								<div class="input"><input type="text" name="nome" value="" size="" /></div>
 								<span class="arrow"></span>
 							</div>
 							<div class="body">
@@ -29,18 +30,18 @@
 								
 									<div class="texto">
 										<label for="link">Link de referência:</label>
-										<div class="input"><input type="text" name="link" value="http://www.gnt.com.br/post-falando-sobre-esse-perfil.html" size="" /></div>
+										<div class="input"><input type="text" name="link" value="" size="" /></div>
 										<label for="texto">Texto do link de referência:</label>
-										<div class="input"><input type="text" name="texto" value="Saiba mais" size="" /></div>
+										<div class="input"><input type="text" name="texto" value="" size="" /></div>
 									</div>
 									<div class="imagem">
 										<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
-										<div class="quadro"><img id="alvo0" src="assets/img/backgrounds/imagem.png" /></div>
 										
-										<form id="fileupload0" action="assets/server/php/" method="POST" enctype="multipart/form-data">
-										<span class="btn btn-success fileinput-button">
-											<input id="file" type="file"  />
-										</span>
+										<form class="fileupload" action="assets/server/php/" method="POST" enctype="multipart/form-data">
+											<div class="quadro"><img id="alvo" src="assets/img/backgrounds/imagem.png" name="imagem" /></div>
+											<span class="btn btn-success fileinput-button">
+												<input id="file" type="file" />
+											</span>
 										</form>
 										
 									</div>
@@ -51,11 +52,12 @@
 								
 									<div class="titulo-respostas">Respostas:</div>
 									
+									<!--A numeração de identificação de #sortable, dos radios e de cada grupo é gerada dinamicamente e precisa ser salva no BD-->
 									<div id="sortable0" class="sorteia">
 										
 												<div class="header">
 													<span class="icon"></span>
-													<div class="input"><input type="text" name="nome" value="Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cui..." size="" /></div>
+													<div class="input"><input type="text" name="nome" value="" size="" /></div>
 													<div class="radio">
 														<label for="radio00" class="radioCustom"></label>
 														<input type="radio" id="radio00" value="0" name="grupo0" />
@@ -65,7 +67,7 @@
 												
 												<div class="header">
 													<span class="icon"></span>
-													<div class="input"><input type="text" name="nome" value="Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cui..." size="" /></div>
+													<div class="input"><input type="text" name="nome" value="" size="" /></div>
 													<div class="radio">
 														<label for="radio10" class="radioCustom"></label>
 														<input type="radio" id="radio10" value="1" name="grupo0" />
@@ -73,11 +75,70 @@
 													</div>
 												</div>
 										
-									</div>
-																	
-								</div><!--respostas-->
-								
+									</div>																	
+								</div><!--respostas-->								
+							</div><!--body-->
+							
+							<div class="header">
+								<span class="icon"></span>
+								<div class="input"><input type="text" name="nome" value="" size="" /></div>
+								<span class="arrow"></span>
 							</div>
+							<div class="body">
+							
+								<div id="perguntas">
+								
+									<div class="texto">
+										<label for="link">Link de referência:</label>
+										<div class="input"><input type="text" name="link" value="" size="" /></div>
+										<label for="texto">Texto do link de referência:</label>
+										<div class="input"><input type="text" name="texto" value="" size="" /></div>
+									</div>
+									<div class="imagem">
+										<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
+										
+										<form class="fileupload" action="assets/server/php/" method="POST" enctype="multipart/form-data">
+											<div class="quadro"><img id="alvo" src="assets/img/backgrounds/imagem.png" name="imagem" /></div>
+											<span class="btn btn-success fileinput-button">
+												<input id="file" type="file" />
+											</span>
+										</form>
+										
+									</div>
+								
+								</div><!--perguntas-->
+								
+								<div id="respostas">
+								
+									<div class="titulo-respostas">Respostas:</div>
+									
+									<!--A numeração de identificação de #sortable e dos radios é gerada dinamicamente e precisa ser salva no BD-->
+									<div id="sortable1" class="sorteia">
+										
+												<div class="header">
+													<span class="icon"></span>
+													<div class="input"><input type="text" name="nome" value="" size="" /></div>
+													<div class="radio">
+														<label for="radio11" class="radioCustom"></label>
+														<input type="radio" id="radio11" value="0" name="grupo1" />
+														Esta é a resposta correta
+													</div>
+												</div>
+												
+												<div class="header">
+													<span class="icon"></span>
+													<div class="input"><input type="text" name="nome" value="" size="" /></div>
+													<div class="radio">
+														<label for="radio21" class="radioCustom"></label>
+														<input type="radio" id="radio21" value="1" name="grupo1" />
+														Esta é a resposta correta
+													</div>
+												</div>
+										
+									</div>																	
+								</div><!--respostas-->								
+							</div><!--body-->
+							
 					</div>
 					
 				</div><!--accordion-->			
