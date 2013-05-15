c5d2994b0cebc62773a67b2d2f9bdd76f7b95ddd
Denise <denise@skidun.com.br>
2013-05-15 15:06:31 -0300

    atualizando

--- perfil1.php
+++ perfil1.php
@@ -20,32 +20,6 @@
 				<div id="accordion">
 					
 					<div class="group">
-							<div class="header">
-								<span class="icon"></span>
-								<div class="input"><input type="text" name="nome" value="Carente" size="" /></div>
-								<span class="arrow"></span>
-							</div>
-							<div class="body">
-								<div class="texto">
-									<label for="descricao">Descrição</label>
-									<div class="textarea"><textarea name="descricao" cols="" rows="">Você é a amiga da galera! Seu tempo livre é todo dedicado a amigos e pessoas queridas, por isso você nem acha que precisa gastar os neurônios pensando em como fazer para encontrar um namorado. Solidão? Que nada!</textarea></div>
-									<label for="link">Link de referência:</label>
-									<div class="input"><input type="text" name="link" value="http://www.gnt.com.br/post-falando-sobre-esse-perfil.html" size="" /></div>
-									<label for="texto">Texto do link de referência:</label>
-									<div class="input"><input type="text" name="texto" value="Saiba mais" size="" /></div>
-								</div>
-								<div class="imagem">
-									<label for="imagem">Imagem relacionada:<span>Dimensões: 240px x 260px</span></label>
-																		
-									<form class="fileupload" action="assets/server/php/" method="POST" enctype="multipart/form-data">
-										<div class="quadro"><img id="alvo" src="assets/img/backgrounds/imagem.png" name="imagem" /></div>
-										<span class="btn btn-success fileinput-button">
-											<input id="file" type="file" />
-										</span>
-									</form>
-									
-								</div>
-							</div>
 							
 							<div class="header">
 								<span class="icon"></span>
