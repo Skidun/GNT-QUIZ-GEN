c3085d48c389f506ad41936f774801cf3039f045
Denise <denise@skidun.com.br>
2013-07-02 15:19:13 -0300

    Tela de visualizacao incorporada em iframe, criacao de slideshow, criacao de botao que chama a etapa de resultados

--- assets/js/main.js
+++ assets/js/main.js
@@ -625,5 +625,62 @@ $(function(){
 		$('html, body').animate({scrollTop:$(document).height()}, 1000);
 		return false;
 	});
-	
+
 });
+
+	/*Calcula o tamanho do Iframe*/
+	$(function(){
+		var altura = $('iframe.janela').contents().find('#quizVisualizacao').height();
+		$('iframe.janela').attr('height',altura);
+	});	
+
+//Lógica do slideshow da Visualização
+$(document).ready(function(){
+  var currentPosition = 0;
+  var slideWidth = 620;
+  var slides = $('.slide');
+  var numberOfSlides = slides.length;
+  
+  // Remove scrollbar in JS
+  $('#slidesContainer').css('overflow', 'hidden');
+  
+  // Wrap all .slides with #slideInner div
+  slides
+  .wrapAll('<div id="slideInner"></div>')
+  // Float left to display horizontally, readjust .slides width
+  .css({
+    'float' : 'left',
+    'width' : slideWidth
+  });
+  
+  // Set #slideInner width equal to total width of all slides
+  $('#slideInner').css('width', slideWidth * numberOfSlides);
+
+  // Hide left arrow control on first load
+  manageControls(currentPosition);
+ 
+  // Create event listeners for .controls clicks
+  $('.control')
+    .bind('click', function(){
+    // Determine new position
+      currentPosition = ($(this).attr('id')=='proximo') 
+    ? currentPosition+1 : currentPosition-1;
+  
+      // Hide / show controls
+      manageControls(currentPosition);
+      // Move slideInner using margin-left
+      $('#slideInner').animate({
+        'marginLeft' : slideWidth*(-currentPosition)
+      });
+    });
+  
+  // manageControls: Hides and shows controls depending on currentPosition
+  function manageControls(position){
+    // Hide left arrow if position is first slide
+    if(position==0){ $('#anterior').hide() }
+    else{ $('#anterior').show() }
+    // Hide right arrow if position is last slide
+    if(position==numberOfSlides-1){ $('#proximo').hide(); $('#chamaResultado').show() } 
+    else{ $('#proximo').show(); $('#chamaResultado').hide() }
+    } 
+  });
---
+++ visualizacao.php
@@ -0,0 +1,64 @@
+<?php require_once('header.php'); ?>
+
+						<div id="quizVisualizacao" class="quiz" style="background:#faab2a;">
+							<div id="nome" style="font-size:20px; color:#333333; text-align:left;">Que tipo de solteira você é?</div>
+
+							<!-- Slideshow HTML -->
+							<div id="slideshow">
+							  <div id="slidesContainer">
+
+							    <div class="slide">
+									<div id="texto">
+										<div class="titulo" style="font-size:24px; color:#333333; text-align:left;">Chegou o fim de semana e você:</div>
+										<div class="subtitulo" style="text-align:left"><a href="#" style="font-size:16px; color:#333333;">Texto do link de referência.</a></div>
+										<table class="respostas" style="font-size:15px; color:#333333; text-align:left;">
+											<tr>
+												<td><input type="radio" name="resposta" value="0" /></td>
+												<td>Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cuidar da casa...</td>
+											</tr>
+											<tr>
+												<td><input type="radio" name="resposta" value="1" /></td>
+												<td>Liga para os amigos e pergunta se alguém tem um cara interessante. e bonito para te apresentar.</td>
+											</tr>
+										</table>
+									</div>
+									<div id="imagem" style="background: #fdd595;">
+										<img id="alvo-perguntas" src="assets/img/backgrounds/imagem2.png" />
+									</div>
+							    </div>
+
+							    <div class="slide">
+									<div id="texto">
+										<div class="titulo" style="font-size:24px; color:#333333; text-align:left;">Chegou o fim de semana e você:</div>
+										<div class="subtitulo" style="text-align:left"><a href="#" style="font-size:16px; color:#333333;">Texto do link de referência.</a></div>
+										<table class="respostas" style="font-size:15px; color:#333333; text-align:left;">
+											<tr>
+												<td><input type="radio" name="resposta" value="0" /></td>
+												<td>Só pensa nas coisas que quer fazer sozinha: pedalar, assistir a um filme, cuidar da casa...</td>
+											</tr>
+											<tr>
+												<td><input type="radio" name="resposta" value="1" /></td>
+												<td>Liga para os amigos e pergunta se alguém tem um cara interessante. e bonito para te apresentar.</td>
+											</tr>
+										</table>
+									</div>
+									<div id="imagem" style="background: #fdd595;">
+										<img id="alvo-perguntas" src="assets/img/backgrounds/imagem2.png" />
+									</div>
+							    </div>
+
+							  </div>
+							</div>
+							<!-- Slideshow HTML -->
+
+							<div id="botoes">
+								<!--Chama slide anterior-->
+								<a href="#" id="anterior" class="control" title="Anterior" style="color:#ffffff; background-color:#cc1e59;">&laquo; Anterior</a>
+								<!--Chama slide posterior-->
+								<a href="#" id="proximo" class="control" title="Próximo" style="color:#ffffff; background-color:#cc1e59;">Próximo &raquo;</a>
+								<!--Chama a página de resultado, só aparece ao final do slideshow-->
+								<a href="#" id="chamaResultado" title="Próximo" style="color:#ffffff; background-color:#cc1e59;">Próximo &raquo;</a>
+							</div>
+						</div>
+
+<?php require_once('footer.php'); ?>
